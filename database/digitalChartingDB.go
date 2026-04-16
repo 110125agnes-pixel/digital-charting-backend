d0aa128fce9e1064b57dac4c4df4a15b886e51da
110125agnes-pixel <your-email@example.com>
2026-04-17 00:13:53 +0800

    feat: create user table

--- database/digitalChartingDB.go
+++ database/digitalChartingDB.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"os"
 
+	"github.com/110125agnes-pixel/digital-charting-backend/models"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -17,7 +19,9 @@ func ConnectToDigitalChartingDB() (*gorm.DB, context.Context) {
 	}
 
 	// *** put all db.AutoMigrate() between this comment ***
-	
+	if err := db.AutoMigrate(&models.User{}); err != nil {
+		panic("Failed to migrate database: " + err.Error())
+	}
 	// *** put all db.AutoMigrate() between this comment ***
 
 	ctx := context.Background()
