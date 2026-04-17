fc2a02bc27a4a6960f6454d173f0ac13c280d2d9
isiahveneracion <isiahveneracion@gmail.com>
2026-04-17 21:54:41 +0800

    feat: Create an API endpoint for getting all records in reference.docmaster table

--- controllers/docMaster.go
+++ controllers/docMaster.go
@@ -1,1 +1,30 @@
 package controllers
+
+import (
+	"net/http"
+
+	"github.com/110125agnes-pixel/digital-charting-backend/database"
+	"github.com/110125agnes-pixel/digital-charting-backend/models"
+	"github.com/gin-gonic/gin"
+)
+
+// GetDocMasters retrieves all records from reference.docmaster using GORM
+func GetDocMasters(c *gin.Context) {
+	db, ctx := database.ConnectToReferenceDB()
+
+	var docMasters []models.DocMaster
+	if err := db.WithContext(ctx).Find(&docMasters).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error":   true,
+			"message": "Failed retrieved doc masters",
+			"result":  nil,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"error":   nil,
+		"message": "Successfully retrieved doc masters",
+		"result":  docMasters,
+	})
+}
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os"
 
+	"github.com/110125agnes-pixel/digital-charting-backend/controllers"
 	"github.com/110125agnes-pixel/digital-charting-backend/database"
 	"github.com/110125agnes-pixel/digital-charting-backend/utils"
 	"github.com/gin-gonic/gin"
@@ -16,11 +17,7 @@ func main() {
 
 	docMastersGroup := router.Group("/api/docMasters")
 	{
-		docMastersGroup.GET("/hello", func (c * gin.Context) {
-			c.JSON(200, gin.H{
-				"message": "Hello World",
-			})
-		})
+		docMastersGroup.GET("/", controllers.GetDocMasters)
 	}
 
 	router.Run(":" + os.Getenv("PORT"))
